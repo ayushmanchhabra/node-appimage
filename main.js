dfdb89983cbf37493734ec19587f155750606fa7
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-04-20 18:23:08 +0530

    feat: create basic AppImage (#2)

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import child_process from 'node:child_process';
+import stream from 'node:stream';
+
+import axios from 'axios';
 
 /**
  * 
  * @param {object} options
- * @param {string} options.appName 
+ * @param {string} options.appName
+ * @param {string} options.outDir
+ * @param {string} options.srcPath
+ * @param {string} options.outPath
+ * @param {object} options.desktopConfig
+ * @param {string} options.desktopConfig.Name
+ * @param {string} options.desktopConfig.Type
+ * @param {string} options.desktopConfig.Comment
+ * @param {string} options.desktopConfig.Exec
+ * @param {string} options.desktopConfig.Icon
+ * @param {string[]} options.desktopConfig.Categories
+ * @returns {Promise<void>} - Resolves when the AppImage is created
  */
-async function appImage({
+export default async function createAppImage({
     appName,
     outDir,
+    srcPath,
+    iconPath,
+    outPath,
+    iconOutPath,
+    desktopConfig = {},
 }) {
-    throw new Error('Not implemented');
+    const appDir = await createAppDirFolder(appName, outDir);
+    await createAppRunScript(appDir);
+    await createDesktopFile(appDir, desktopConfig);
+    await placeFile(appDir, srcPath, outPath);
+    await placeFile(appDir, iconPath, iconOutPath);
+    const appImageToolPath = path.resolve(appDir, 'appimagetool.AppImage')
+    if (!fs.existsSync(appImageToolPath)) {
+        await downloadAppImageTool(appImageToolPath);
+    }
+    await fs.promises.chmod(appImageToolPath, 0o755);
+    child_process.execSync(`${appImageToolPath} ${appDir}`);
 }
 
 /**
  * Creates a directory for the AppDir.
- * @param {*} appName - Name of the application
- * @param {*} outDir - Output directory for the AppDir
+ * @param {string} appName - Name of the application
+ * @param {string} outDir - Output directory for the AppDir
  * @returns {Promise<string>} - Absolute file path of AppDir directory
  * @throws {Error} If the AppDir already exists
  */
@@ -35,5 +65,78 @@ export async function createAppDirFolder(appName, outDir) {
     return appDir;
 }
 
-export default appImage;
+/**
+ * Create AppRun script.
+ * @param {string} appDir - Absolute file path of AppDir directory
+ * @param {string} appRunScript - AppRun script content
+ * @returns {Promise<void>} - Resolves when the AppRun script is created
+ */
+export async function createAppRunScript(appDir, appRunScript = '') {
+    /**
+     * @type {string}
+     */
+    const appRunPath = path.resolve(appDir, 'AppRun');
 
+    if (appRunScript === '') {
+        appRunScript = `#!/bin/sh
+HERE="$(dirname "$(readlink -f "\${0}")")"
+export PATH="\${HERE}/usr/bin:\${PATH}"
+exec test "$@"`;
+    }
+
+    await fs.promises.writeFile(appRunPath, appRunScript);
+    await fs.promises.chmod(appRunPath, 0o755);
+}
+
+export async function createDesktopFile(appDir, desktopConfig) {
+    const desktopFilePath = path.resolve(appDir, `${desktopConfig.Name}.desktop`);
+    const desktopFileContent = `[Desktop Entry]
+Type=${desktopConfig.Type}
+Name=${desktopConfig.Name}
+Comment=${desktopConfig.Comment}
+Exec=${desktopConfig.Exec}
+Icon=${desktopConfig.Icon}
+Categories=${desktopConfig.Categories.join(';')}`;
+
+    await fs.promises.writeFile(desktopFilePath, desktopFileContent);
+}
+
+/**
+ * 
+ * @param {string} appDir - Absolute file path of AppDir directory
+ * @param {string} srcPath - File path to binary
+ * @param {string} outPath - Filepath to place the binary
+ * @returns {Promise<void>} - Resolves when the file is placed
+ */
+export async function placeFile(appDir, srcPath, outPath) {
+
+    /**
+     * @type {string}
+     */
+    const dirPath = path.resolve(appDir, path.dirname('.' + outPath));
+
+    await fs.promises.mkdir(dirPath, { recursive: true });
+    await fs.promises.copyFile(path.resolve(srcPath), path.resolve(appDir, '.' + outPath));
+
+}
+
+export async function downloadAppImageTool(filePath) {
+   
+    let apiResponse = await axios({
+        method: 'get',
+        url: 'https://api.github.com/repos/appimage/appimagetool/releases',
+        responseType: 'json'
+    });
+    
+    let url = `https://github.com/AppImage/appimagetool/releases/download/${apiResponse.data[0].tag_name}/appimagetool-${process.arch === 'x64' ? 'x86_64': 'i686'}.AppImage`
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    const response = await axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+    });
+
+    await stream.promises.pipeline(response.data, writeStream);
+}
--- main.test.js
+++ main.test.js
@@ -3,7 +3,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { afterEach, describe, it } from 'node:test';
 
-import { createAppDirFolder } from './main.js';
+import createAppImAge, { createAppDirFolder, createAppRunScript, placeFile, createDesktopFile, downloadAppImageTool } from './main.js';
 
 describe('AppImage test suite', function () {
 
@@ -20,8 +20,59 @@ describe('AppImage test suite', function () {
         );
     })
 
+    it('creates an AppRun script', async function () {
+        await fs.promises.mkdir('./test.AppDir', { recursive: true });
+        await createAppRunScript('./test.AppDir', '');
+        assert.strictEqual(fs.existsSync('./test.AppDir/AppRun'), true);
+    });
+
+    it('places the binary file into {appName}.AppDir directory', async function () {
+        await fs.promises.mkdir('./test.AppDir', { recursive: true });
+        await placeFile('./test.AppDir', './test', '/usr/bin/test');
+        assert.strictEqual(fs.existsSync('./test.AppDir/usr/bin/test'), true);
+    });
+
+    it('creates desktop file', async function () {
+        await fs.promises.mkdir('./test.AppDir', { recursive: true });
+        await createDesktopFile('./test.AppDir', {
+            Type: 'Application',
+            Name: 'Test',
+            Comment: 'Test application',
+            Exec: 'test',
+            Icon: 'test',
+            Categories: ['Utility'],
+        });
+        assert.strictEqual(fs.existsSync('./test.AppDir/Test.desktop'), true);
+    });
+
+    it('downloads appimagetool from GitHub', async function () {
+        await downloadAppImageTool('./appimagetool.AppImage');
+        assert.strictEqual(fs.existsSync('./appimagetool.AppImage'), true);
+    });
+
+    it('creates an {appName}.AppImage', async function () {
+        await createAppImAge({
+            appName: 'test',
+            outDir: '.',
+            srcPath: './test',
+            iconPath: './test.png',
+            outPath: '/usr/bin/test',
+            iconOutPath: '/test.png',
+            desktopConfig: {
+                Type: 'Application',
+                Name: 'Test',
+                Comment: 'Test application',
+                Exec: 'test',
+                Icon: 'test',
+                Categories: ['Utility'],
+            },
+        });
+
+        assert.strictEqual(fs.existsSync('./Test-x86_64.AppImage'), true);
+    });
+
     afterEach(async function () {
-        // Clean up the test directory after `creates an {appName}.AppDir` test
+        // Clean up the test fixture test.AppDir directory after every test
         await fs.promises.rm('./test.AppDir', { recursive: true, force: true });
     });
 });
