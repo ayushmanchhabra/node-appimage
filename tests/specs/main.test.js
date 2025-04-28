f150ba6ef25c66d37ad7a736c983177d73eeeb3f
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-04-29 00:37:36 +0530

    chore(test): ./demo -> ./test/

---
+++ tests/specs/main.test.js
@@ -0,0 +1,78 @@
+import assert from 'node:assert';
+import fs from 'node:fs';
+import path from 'node:path';
+import { afterEach, describe, it } from 'node:test';
+
+import createAppImAge, { createAppDirFolder, createAppRunScript, placeFile, createDesktopFile, downloadAppImageTool } from '../../main.js';
+
+describe('AppImage test suite', function () {
+
+    it('creates an {appName}.AppDir', async function () {
+        await createAppDirFolder('demo', './tests/fixtures');
+        assert.strictEqual(fs.existsSync('./tests/fixtures/demo.AppDir'), true);
+    });
+
+    it('throws error if {appName}.AppDir exists', async function () {
+        await fs.promises.mkdir('./tests/fixtures/demo.AppDir', { recursive: true });
+        assert.rejects(
+            createAppDirFolder('demo', './tests/fixtures'),
+            new Error(`AppDir at file path ${path.resolve('./tests/fixtures/demo.AppDir')} already exists.`),
+        );
+    })
+
+    it('creates an AppRun script', async function () {
+        await fs.promises.mkdir('./tests/fixtures/demo.AppDir', { recursive: true });
+        await createAppRunScript('./tests/fixtures/demo.AppDir', '');
+        assert.strictEqual(fs.existsSync('./tests/fixtures/demo.AppDir/AppRun'), true);
+    });
+
+    it('places the binary file into {appName}.AppDir directory', async function () {
+        await fs.promises.mkdir('./tests/fixtures/demo.AppDir', { recursive: true });
+        await placeFile('./tests/fixtures/demo.AppDir', './tests/fixtures/demo', '/usr/bin/demo');
+        assert.strictEqual(fs.existsSync('./tests/fixtures/demo.AppDir/usr/bin/demo'), true);
+    });
+
+    it('creates desktop file', async function () {
+        await fs.promises.mkdir('./tests/fixtures/demo.AppDir', { recursive: true });
+        await createDesktopFile('./tests/fixtures/demo.AppDir', {
+            Type: 'Application',
+            Name: 'test',
+            Comment: 'Test application',
+            Exec: 'demo',
+            Icon: 'demo',
+            Categories: ['Utility'],
+        });
+        assert.strictEqual(fs.existsSync('./tests/fixtures/demo.AppDir/test.desktop'), true);
+    });
+
+    it('downloads appimagetool from GitHub', async function () {
+        await downloadAppImageTool('./tests/fixtures/appimagetool.AppImage');
+        assert.strictEqual(fs.existsSync('./tests/fixtures/appimagetool.AppImage'), true);
+    });
+
+    it('creates an {appName}.AppImage', async function () {
+        await createAppImAge({
+            appName: 'demo',
+            outDir: './tests/fixtures/demo.AppDir',
+            srcPath: './tests/fixtures/demo',
+            iconPath: './tests/fixtures/demo.png',
+            outPath: '/usr/bin/demo',
+            iconOutPath: '/demo.png',
+            desktopConfig: {
+                Type: 'Application',
+                Name: 'demo',
+                Comment: 'Demo application',
+                Exec: 'demo',
+                Icon: 'demo',
+                Categories: ['Utility'],
+            },
+        });
+
+        assert.strictEqual(fs.existsSync('./demo-x86_64.AppImage'), true);
+    });
+
+    afterEach(async function () {
+        // Clean up the test fixture test.AppDir directory after every test
+        await fs.promises.rm('./tests/fixtures/demo.AppDir', { recursive: true, force: true });
+    });
+});
